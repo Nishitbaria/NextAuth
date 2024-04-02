f2ced7e8fd6431a7f2db6e411233e32e1fcd711e
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2024-04-02 20:14:34 +0530

    Update token verification secret

--- nextauth/utils/getDataFromToken.ts
+++ nextauth/utils/getDataFromToken.ts
@@ -4,7 +4,8 @@ import jwt from "jsonwebtoken";
 export const getDataFromToken = (request: NextRequest) => {
     try {
         const token = request.cookies.get("token")?.value || '';
-        const decodedToken: any = jwt.verify(token, process.env.TOKEN_SECRET!);
+        console.log(token);
+        const decodedToken: any = jwt.verify(token, process.env.NEXTAUTH_SECRET!);
         return decodedToken.id;
     } catch (error: any) {
         console.error(error.message);
